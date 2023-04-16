ecb72586c3235bad034f2921c24df72586d7f01a
zikaloai71 <ze.lo.moukhtar@el-eng.menofia.edu.eg>
2023-04-17 00:13:17 +0200

    add services to store tokens and add api to collect all the apis and custom hooks to use it

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,92 +8,12 @@ import SignUp from "./pages/SignUp";
 import Product from "./components/Product/Product";
 import PrivateRoute from "./components/PrivateRoute";
 import { ProductsContextProvider } from "./Context/ProductsContext";
-import {ApolloProvider, ApolloClient, InMemoryCache,  fromPromise ,gql , createHttpLink} from "@apollo/client";
-import { onError } from '@apollo/client/link/error'
-import { setContext } from "@apollo/client/link/context";
+import { ApolloProvider } from "@apollo/client";
+import {client} from "./apollo/setup";
 import ProfileDirect from "./pages/ProfileDirect";
 import Cart from "./pages/Cart";
-import Cookies from "universal-cookie";
-
 
 function App() {
-const cookies = new Cookies();
-  
-const errorLink = onError( ({ graphQLErrors, operation, forward }) => {
-  if (graphQLErrors) {
-      const messages = graphQLErrors.map(({ message }) => message);
-      const refreshToken = cookies.get("refreshToken");
-      if (messages.includes('Signature has expired') || messages.includes('Error decoding signature')) {
-          return getNewTokenByRefreshToken(refreshToken)
-              .filter(value => Boolean(value))
-              .flatMap((newToken) => {
-                  const oldHeaders = operation.getContext().headers;
-                  operation.setContext({
-                      headers: {
-                          ...oldHeaders,
-                          authorization: `JWT ${newToken}`,
-                      },
-                  });
-                  return forward(operation);
-              });
-      }
-  }
-})
-
-const authLink = setContext((_, { headers }) => {
-  const token = cookies.get("token");
-  return {
-      headers: {
-          ...headers,
-          authorization:  token ? `JWT ${token}` : '',
-      },
-  };
-});
-
-const getNewTokenByRefreshToken = (refreshToken) => {
-  return fromPromise(
-      client
-          .mutate({
-              mutation:gql`
-              mutation refreshToken($refreshToken: String!) {
-                refreshToken(refreshToken: $refreshToken) {
-                  token
-                  refreshToken
-                  payload
-                }
-             }`,
-              variables: { refreshToken },
-          })
-          .then((response) => {
-              if (response?.data) {
-                  const { token, refreshToken } = response.data.refreshToken;
-                  if (token && refreshToken) {
-                      cookies.set('token',token);
-                      cookies.set('refreshToken',refreshToken);
-                      return token;
-                  }
-              }
-          })
-          .catch((error) => {
-              if (error.message === 'Invalid refresh token' || error.message === 'Refresh token is expired') {
-                  cookies.remove('token');
-                  cookies.remove('refreshToken');
-                  window.location = window.location.origin;
-              }
-          })
-  );
-};
-
-
-  const httpLink = createHttpLink({
-    uri: 'http://localhost:8000/graphql/',
-  });
- 
-  //connect with server
-  const client = new ApolloClient({
-    link: errorLink.concat(authLink).concat(httpLink),
-    cache: new InMemoryCache()
-  });
 
   return (
     <ApolloProvider client={client}>
