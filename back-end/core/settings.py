5befd4ac9cc0d5c949eea14056a2570d6c837429
rabeeh rabie <bohaaua@gmail.com>
2023-04-17 16:10:37 +0200

    handle auth

--- back-end/core/settings.py
+++ back-end/core/settings.py
@@ -193,8 +193,11 @@ AUTHENTICATION_BACKENDS = [
 #                  |_|
 GRAPHQL_JWT = {
     'JWT_VERIFY_EXPIRATION': True,
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_LONG_RUNNING_REFRESH_TOKEN': True,
     'JWT_EXPIRATION_DELTA': timedelta(minutes=120),
     'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
+    'JWT_HIDE_TOKEN_FIELDS': True,
 }
 GRAPHENE = {
     'SCHEMA': 'api.graphql.schema',
