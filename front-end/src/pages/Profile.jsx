fb621edf29e9496760e6651dcf2ff318de436d17
Ramyahmed225 <ramiahmed2a2u@gmail.com>
2023-05-09 19:04:15 +0300

    editing profile page by adding address and message option

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -11,7 +11,6 @@ export default function Profile() {
     <div className="container mx-auto px-4">
       <div className="profile-header flex items-center justify-between mb-4">
         <h1 className="text-3xl font-bold">Profile</h1>
-        
       </div>
       <div className="profile-info bg-white shadow-md p-6 rounded-md mb-4">
         <div className="info-row mb-4">
@@ -24,8 +23,8 @@ export default function Profile() {
         </div>
       </div>
       <div className="profile-options bg-white shadow-md p-6 rounded-md">
-        <h2 className="text-2xl font-bold mb-6">Your Account</h2>
-        <div className="grid grid-cols-3 gap-4">
+        <h2 className="text-2xl font-bold mb-4">Your Account</h2>
+        <div className="grid grid-cols-3 gap-4 mt-4">
           <div className="flex items-center space-x-2">
             <FiPackage className="text-3xl text-gray-700" />
             <Link to="/orders" className="text-gray-800 hover:text-blue-500 transition-colors duration-300">Orders</Link>
@@ -43,8 +42,8 @@ export default function Profile() {
             <Link to="/change-username" className="text-gray-800 hover:text-blue-500 transition-colors duration-300">Change Username</Link>
           </div>
           <div className="flex items-center space-x-2">
-            <FiLock className="text-3xl text-gray-700" />
-            <Link to="/change-password" className="text-gray-800 hover:text-blue-500 transition-colors duration-300">Change Password</Link>
+            <FiLock className="text-3xl text-gray-700"  />
+            <Link to="/change-password" className="text-gray-800 hover:text-blue-500 transition-colors duration-300" >Change Password</Link>
           </div>
         </div>
       </div>
