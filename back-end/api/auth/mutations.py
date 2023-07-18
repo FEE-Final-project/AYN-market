be3f22d9e3d636ba3e9255b4908a98015d2444ad
rabeeh rabie <bohaaua@gmail.com>
2023-07-18 11:08:23 +0300

    add super user

--- back-end/api/auth/mutations.py
+++ back-end/api/auth/mutations.py
@@ -19,6 +19,10 @@ from apps.user.models import User
 from django.utils.translation import gettext as _
 from .types import AuthType
 from api.customer.types import CustomerType
+from graphql_jwt.decorators import (
+    login_required,
+    user_passes_test
+)
 
 
 #      _         _   _
@@ -39,3 +43,84 @@ class ObtainJSONWebToken(graphql_jwt.JSONWebTokenMutation):
         user = info.context.user
         return cls(user=user, success=True, errors=None)
 
+class AdminUserCreate(relay.ClientIDMutation):
+    """
+    Admin can create superUser
+    """
+
+    admin = graphene.Field(AuthType)
+
+    class Input:
+        password = graphene.String(required=True)
+        password_confirmation = graphene.String(required=True)
+        email = graphene.String(required=True)
+        username=graphene.String(required=True)
+        gender = graphene.String()
+        phone = graphene.String()
+
+    success = graphene.Boolean()
+    errors = graphene.List(graphene.String)
+
+    @login_required
+    def mutate_and_get_payload(
+        root,
+        info,
+        **input
+    ):
+        errors = []
+
+        email = input.get('email').lower().strip()
+        phone = input.get('phone' , None)
+        username=input.get('username')
+        password=input.get('password')
+        password_confirmation=input.get('password_confirmation')
+        if password != password_confirmation:
+            errors.append(
+                _('password and password confirmation does not match!')
+            )
+
+            return CustomerSignUp(success=False, errors=errors)
+
+        email_has_account = User.objects.filter(email=email).exists()
+        if email_has_account:
+            errors.append(
+                _('User with these email already exist!')
+            )
+            return CustomerSignUp(success=False, errors=errors)
+
+        phone_has_account = User.objects.filter(phone=phone).exists()
+        if phone and phone_has_account:
+            errors.append(
+                _('User with these phone already exist!')
+            )
+
+            return CustomerSignUp(success=False, errors=errors)
+
+        username_has_account=User.objects.filter(username=username).exists()
+        if username_has_account:
+            errors.append(
+                _('User with these username already exist!')
+            )
+
+            return CustomerSignUp(success=False, errors=errors)
+
+        try:
+            admin = User.objects.create_superuser(
+                email=email,
+                username=username
+            )
+            if phone:
+                customer.phone = phone
+            if input.get('gender'):
+                customer.gender=input.get('gender')
+
+            admin.set_password(input.get('password'))
+
+            admin.is_active=True
+
+            admin.save()
+
+            return AdminUserCreate(admin=admin, success=True)
+        except Exception as e:
+            errors += [e]
+            return AdminUserCreate(success=False, errors=errors)
